0f09b3dbbff342664ee263db2f171069a9ed95d2
mohammed asif <mohammed.asif@techhack.ca>
2021-10-28 15:33:40 +0600

    kaj shesh

--- src/Component/Pages/Page1.js
+++ src/Component/Pages/Page1.js
@@ -8,7 +8,7 @@ import {
   Grid,
   Radio,
   RadioGroup,
-  Typography
+  Typography,
 } from "@material-ui/core";
 import React, { useState } from "react";
 import allQuestion from "../../JSON/personalityTestQuestion.json";
@@ -21,43 +21,50 @@ export default function Page1() {
   const [curQuestionNo, setCurQuestionNo] = useState(0);
   const [category, setCategory] = useState("");
   const [result, setResult] = useState();
-  
+  const [extraversion, setExtraversion] = useState(0);
+  const [agreeableness, setAgreeableness] = useState(0);
+  const [conscientiousness, setConscientiousness] = useState(0);
+  const [neuroticism, setNeuroticism] = useState(0);
+  const [openness, setOpenness] = useState(0);
+  console.log("extraversion = ", extraversion);
+  console.log("agreeableness = ", agreeableness);
+  console.log("conscientiousness = ", conscientiousness);
+  console.log("neuroticism = ", neuroticism);
+  console.log("openness = ", openness);
+
   const handleChange = (event) => {
     setValue(event.target.value);
   };
-const countResult = () => {
-  let extraversion = 0;
-  let agreeableness = 0;
-  let conscientiousness = 0;
-  let neuroticism = 0;
-  let openness = 0;
-  if (category === "extraversion") {
-    extraversion = +value + extraversion;
-    return extraversion;
-  }
-  else  if (category === "agreeableness") {
-    agreeableness = +value + agreeableness;
-    return agreeableness;
-  }
-  else  if (category === "conscientiousness") {
-    conscientiousness = +value + conscientiousness;
-    return conscientiousness;
-  }
-  else  if (category === "neuroticism") {
-    neuroticism = +value + neuroticism;
-    return neuroticism;
-  }
-  else  if (category === "openness") {
-    openness = +value + openness;
-    return openness;
-  }
-  }
-  countResult()
+  const countResult = () => {
+    switch (category) {
+      case "extraversion":
+        setExtraversion(+value + extraversion);
+        break;
+      case "agreeableness":
+        setAgreeableness(+value + agreeableness);
+        break;
+      case "conscientiousness":
+        setConscientiousness(+value + conscientiousness);
+        break;
+      case "neuroticism":
+        setNeuroticism(+value + neuroticism);
+        break;
+      case "openness":
+        setOpenness(+value + openness);
+        break;
+        deafault:;
+    }
+  };
+  // countResult();
   const nextQuestion = () => {
-    setCurQuestionNo(questionSet[curQuestionNo].index)
-    setValue('');
-  }
-  console.log("point-",value,"step-",category,"RES-",countResult());
+    countResult();
+    setCurQuestionNo(questionSet[curQuestionNo].index);
+    setValue("");
+  };
+  const testResult = () => {
+    
+  };
+  // console.log("point-", value, "step-", category, "RES-", countResult());
   return (
     <div>
       <Card className={classes.card}>
@@ -67,8 +74,19 @@ const countResult = () => {
               <Typography variant="h6" className={classes.title}>
                 Personality Test
               </Typography>
-              <FormControl component="fieldset" className={classes.form} onClick={()=>setCategory(questionSet[curQuestionNo].category)}>
-                <FormLabel component="legend" style={{color:"black", fontWeight:"bold", fontSize:"20px"}}>
+              <FormControl
+                component="fieldset"
+                className={classes.form}
+                onClick={() => setCategory(questionSet[curQuestionNo].category)}
+              >
+                <FormLabel
+                  component="legend"
+                  style={{
+                    color: "black",
+                    fontWeight: "bold",
+                    fontSize: "20px",
+                  }}
+                >
                   {questionSet[curQuestionNo].question}
                 </FormLabel>
                 <RadioGroup
@@ -79,25 +97,36 @@ const countResult = () => {
                 >
                   <FormControlLabel
                     value={questionSet[curQuestionNo].veryInaccurate}
-                    checked={value === questionSet[curQuestionNo].veryInaccurate}
+                    checked={
+                      value === questionSet[curQuestionNo].veryInaccurate
+                    }
                     control={<Radio />}
                     label="Very Inaccurate"
                   />
                   <FormControlLabel
                     value={questionSet[curQuestionNo].moderatelyInaccurate}
-                    checked={value === questionSet[curQuestionNo].moderatelyInaccurate}
+                    checked={
+                      value === questionSet[curQuestionNo].moderatelyInaccurate
+                    }
                     control={<Radio />}
                     label="Moderately Inaccurate"
                   />
                   <FormControlLabel
-                    value={questionSet[curQuestionNo].neitherAccurateNorInaccurate}
+                    value={
+                      questionSet[curQuestionNo].neitherAccurateNorInaccurate
+                    }
                     control={<Radio />}
-                    checked={value === questionSet[curQuestionNo].neitherAccurateNorInaccurate}
+                    checked={
+                      value ===
+                      questionSet[curQuestionNo].neitherAccurateNorInaccurate
+                    }
                     label="Neither Accurate Nor Inaccurate"
                   />
                   <FormControlLabel
                     value={questionSet[curQuestionNo].moderatelyAccurate}
-                    checked={value === questionSet[curQuestionNo].moderatelyAccurate}
+                    checked={
+                      value === questionSet[curQuestionNo].moderatelyAccurate
+                    }
                     control={<Radio />}
                     label="Moderately Accurate"
                   />
@@ -108,11 +137,15 @@ const countResult = () => {
                     label="Very Accurate"
                   />
                 </RadioGroup>
-                {
-                  curQuestionNo < questionSet.length-1 ?
-                    <Button variant="outlined" onClick={nextQuestion}>Next</Button>:
-                    <Button variant="outlined">Finish</Button>
-                }
+                {curQuestionNo < questionSet.length - 1 ? (
+                  <Button variant="outlined" onClick={nextQuestion}>
+                    Next
+                  </Button>
+                ) : (
+                  <Button variant="outlined" onClick={countResult}>
+                    Finish
+                  </Button>
+                )}
               </FormControl>
             </Grid>
           </Grid>
