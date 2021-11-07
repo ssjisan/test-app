40b9918ee4f9ef22eac205ab8f96f7fc1fe942bc
ssjisan <sakibfset13@gmail.com>
2021-11-08 00:03:44 +0600

    form validation

--- src/Component/Pages/Page3.js
+++ src/Component/Pages/Page3.js
@@ -1,13 +1,161 @@
-import { Typography } from '@material-ui/core'
+import { Typography,OutlinedInput,Card,Button,CardActions,CardContent,Grid,TextField,FormControl,IconButton,InputAdornment,InputLabel } from '@material-ui/core'
 import React from 'react'
 import useStyles from "./Style/Page1style";
-
+import { Visibility, VisibilityOff } from "@material-ui/icons";
+import CancelIcon from '@material-ui/icons/Cancel';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 export default function Page3() {
     const classes = useStyles();
-
+    const [values, setValues] = React.useState({
+        password: '',
+      });
+    
+      const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+      console.log(values);
+    
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+    
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
     return (
         <div className={classes.about}>
-                          
-        </div>
+      <Card className={classes.card}>
+        <form>
+          <CardContent>
+            <Grid container spacing={0}>
+              <Grid item lg={6} xs={12}>
+                <TextField
+                  id="fname"
+                  name="fname"
+                  label="First Name*"
+                  className={classes.textField}
+                  margin="normal"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item lg={6} xs={12}>
+              <TextField
+                  id="lname"
+                  name="lname"
+                  label="Last Name*"
+                  className={classes.textField}
+                  margin="normal"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item lg={6} xs={12}>
+              <TextField
+                  id="email"
+                  name="email"
+                  type="email"
+                  label="Email*"
+                  className={classes.textField}
+                  margin="normal"
+                  variant="outlined"
+                />
+                <br/>
+              <br/>
+              <br/>
+               </Grid> 
+              <Grid item lg={6} xs={12}>
+              <TextField
+                  id="phone"
+                  name="phone"
+                  type="text"
+                  label="Phone Number*"
+                  className={classes.textField}
+                  margin="normal"
+                  variant="outlined"
+                />
+                <br/>
+              <br/>
+              <br/>
+              </Grid>
+              <Grid item lg={6} xs={12}>
+              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined"    className={classes.textField}>
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+                </Grid>
+              {/* <Grid item lg={6} xs={12}>
+              <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined"    className={classes.textField}>
+          <InputLabel htmlFor="outlined-adornment-password" className={classes.textField}>Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+              </Grid> */}
+            </Grid>
+            <br/>
+            <br/>
+            {
+                values.password.length >= 8 ? <Typography style={{color:"green"}}> <CheckCircleIcon/>At least 8 characters</Typography> :  <Typography  style={{color:"red"}}> <CancelIcon/> At least 8 characters</Typography>
+            }
+            {
+                values.password.match(/[A-Z]/) ? <Typography style={{color:"green"}}> <CheckCircleIcon/>At least 1 uppercase letter</Typography> :  <Typography  style={{color:"red"}}> <CancelIcon/> At least 8 characters</Typography>
+            }
+            {
+                values.password.match(/[a-z]/) ? <Typography style={{color:"green"}}> <CheckCircleIcon/>At least 1 lowercase letter</Typography> :  <Typography  style={{color:"red"}}> <CancelIcon/> At least 8 characters</Typography>
+            }
+            {
+                values.password.match(/[\d`~!@#$%\^&*()+=|;:'",.<>\/?\\\-]/) ? <Typography style={{color:"green"}}> <CheckCircleIcon/>At least 8 characters</Typography> :  <Typography  style={{color:"red"}}> <CancelIcon/> At least 1 number or special character</Typography>
+            }
+          </CardContent>
+          <CardActions>
+            <Button
+              color="primary"
+              variant="contained"
+              type="submit"
+              className={classes.submit}
+            >
+              SUBMIT
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+    </div>            
     )
 }
--- src/Component/Pages/Style/Page1style.js
+++ src/Component/Pages/Style/Page1style.js
@@ -28,5 +28,130 @@ export default makeStyles((theme) => ({
         width:'100%',
         position:"absolute",
 
-    }
+    }, error: {
+        verticalAlign: 'middle'
+      },
+      headline: {
+        fontWeight: 700,
+        textAlign: 'center',
+        paddingTop: '30px',
+      },
+      subheadline: {
+        fontWeight: 500,
+        textAlign: 'center',
+        paddingTop: '10px',
+        color: '#ccc'
+      },
+      title: {
+        marginTop: theme.spacing(2),
+        color: theme.palette.openTitle
+      },
+      textField: {
+        // margin: '0 2%',
+        width: 300,
+        border: '#ccc',
+        width: '95%',
+        height: '45px',
+      },
+      submit: {
+        margin: 'auto',
+        marginBottom: theme.spacing(2),
+        border: '1px solid #00756A',
+        fontWeight: 500,
+        width:'350px',
+        '&:hover': {
+          border: '1px solid #00756A',
+          backgroundColor: '#fff',
+          color: '#000',
+          fontWeight: 500,
+        }
+      },
+      orDivider: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      seperator: {
+        height: '2px',
+        width: '20vw',
+        backgroundColor: "#000"
+      },
+      orStyle: {
+        padding: '0 5%'
+      },
+      loginWithText: {
+        padding: '0 2%',
+        fontWeight: 700
+      },
+    
+      googleLogin: {
+        width: '270px',
+        height: '45px',
+        borderRadius: '6px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        boxShadow: '1px 1px 5px #ccc',
+        '&:hover': {
+          backgroundColor: '#dc3545',
+          color: '#fff'
+        }
+    
+      },
+      fbLogin: {
+        width: '270px',
+        height: '45px',
+        borderRadius: '6px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        boxShadow: '1px 1px 5px #ccc',
+        '&:hover': {
+          backgroundColor: '#18ACFE',
+          color: '#fff'
+        }
+    
+      },
+      githubLogin: {
+        width: '270px',
+        height: '45px',
+        borderRadius: '6px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        boxShadow: '1px 1px 5px #ccc',
+        '&:hover': {
+          backgroundColor: '#444',
+          color: '#fff'
+        }
+    
+      },
+      loginwithContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginTop: '2%',
+        flexWrap:'wrap'
+      },
+      fbIcon: {
+        color: '#0163E0'
+      },
+
+      tabsDiv:{
+        display: 'flex',
+        justifyContent: 'space-around'
+      },
+      talentTab:{
+        color: '#00756A',
+        borderBottom: '3px solid #00756A',
+        padding:"10px 20px"
+      },
+      companyTab:{
+        color: '#444',
+        borderBottom: '3px solid #fff',
+        padding:"10px 20px"
+      },
 }))
